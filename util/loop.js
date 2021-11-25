86d7fa848408011a2bb6c9bfab266a89209aabbc
Jessica <koda@horizons.gg>
2021-11-25 11:04:09 +1000

    SEO Changes

--- util/loop.js
+++ util/loop.js
@@ -5,8 +5,11 @@ const fetch = require('node-fetch')
 
 async function Loop() {
 
-    process.cache.topVoters = await fetch(`https://space-engineers.com/api/?object=servers&element=voters&key=${process.env.api.se}&month=current&format=json&limit=10`).then(res => res.json())
-    process.cache.serverData = await fetch(`https://space-engineers.com/api/?object=servers&element=detail&key=${process.env.api.se}`).then(res => res.json())
+    process.cache.topVoters = await fetch(`https://space-engineers.com/api/?object=servers&element=voters&key=${process.env.api.se}&month=current&format=json&limit=10`).then(res => res.json()).catch(err => console.log(err))
+    if (!process.cache.topVoters) process.cache.topVoters = { voters: [] }
+
+    process.cache.serverData = await fetch(`https://space-engineers.com/api/?object=servers&element=detail&key=${process.env.api.se}`).then(res => res.json()).catch(err => console.log(err))
+    if (!process.cache.serverData) process.cache.serverData = { rank: '0' }
 
 }
 setInterval(Loop, 1000 * 30)
