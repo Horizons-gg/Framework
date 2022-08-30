c3dc01db13934bb6b2f0111bd0e961598d04ae83
Koda <koda@horizons.gg>
2022-08-30 19:13:24 +1000

    centered text on 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,11 +5,9 @@ import Head from 'next/head'
 const NoPageFoundPage: NextPage = () => {
 
     return (
-        
-        <div className='bg-slate-900 h-screen text-center'>
 
-                <span className='text-slate-100 text-8xl font-bold'>Nothing Here, Go Away</span>
-
+        <div className='bg-slate-900 flex h-screen justify-center'>
+            <span className='text-slate-100 text-8xl font-bold m-auto'>Nothing Here, Go Away</span>
         </div>
 
     )
