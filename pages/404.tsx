da25fdc33942a1e108163a6321862777bfd78ce1
Index <callummonks21@gmail.com>
2022-08-31 16:49:18 +1000

    Update 404.tsx
    -Added Backround Image With Blur
    -Changed Color of Text Hover

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,11 +8,11 @@ const NoPageFoundPage: NextPage = () => {
         <div className='h-screen overflow-hidden'>
             <div className='h-screen w-screen fixed flex justify-center select-none z-10'>
                 <Link href='/'>
-                    <a className='text-slate-100 text-8xl font-bold m-auto hover:text-pink-500 transition-all'>404, Page Not Found</a>
+                    <a className='text-slate-100 text-8xl font-bold m-auto hover:text-stone-400 transition-all'>404, Page Not Found</a>
                 </Link>
             </div>
 
-            <div className='h-screen' style={{ backgroundImage: `url('/images/landingImage.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center' }} />
+            <div className='h-screen blur-sm scale-105' style={{ backgroundImage: `url('/images/landingImage.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center' }} />
         </div>
     )
 }
